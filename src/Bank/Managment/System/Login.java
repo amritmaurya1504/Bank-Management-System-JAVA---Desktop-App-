e64499761f0341a6edc0708c964e49a100c48e42
Amrit Raj <amrit.raj1504@gmail.com>
2023-07-31 02:19:32 +0530

    Java + Java Swing + MySQL + AWT

---
+++ src/Bank/Managment/System/Login.java
@@ -0,0 +1,97 @@
+package Bank.Managment.System;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame implements ActionListener {
+    JButton login, register, clear; // Global Variable
+    JTextField cardTextField;
+    JPasswordField pinTextField;
+
+    Login(){
+        setTitle("Automated Teller Machine");
+        setLayout(null);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/bank.png"));
+        // Resizing Image
+        Image i2 = i1.getImage().getScaledInstance(70,70, Image.SCALE_DEFAULT);
+        ImageIcon newImage = new ImageIcon(i2);
+        // we have to place icon on frame using JLable
+        JLabel label = new JLabel(newImage);
+        label.setBounds(70,10,100,100); // to use this we have to make null border laylout
+        add(label);
+
+        JLabel text = new JLabel("Welcome to State Bank Of India");
+        text.setFont(new Font("Oswald", Font.BOLD, 24));
+        text.setBounds(170,60,400,40);
+        add(text);
+
+        JLabel cardNo = new JLabel("Enter Your Card No : ");
+        cardNo.setFont(new Font("Oswald", Font.ITALIC, 15));
+        cardNo.setBounds(130,120,400,40);
+        add(cardNo);
+
+        cardTextField = new JTextField();
+        cardTextField.setFont(new Font("Arial", Font.PLAIN, 15));
+        cardTextField.setBounds(130,160,400,40);
+        add(cardTextField);
+
+
+        JLabel atmPin = new JLabel("Enter Your ATM Pin : ");
+        atmPin.setFont(new Font("Arial", Font.PLAIN, 15));
+        atmPin.setFont(new Font("Oswald", Font.ITALIC, 15));
+        atmPin.setBounds(130,200,400,40);
+        add(atmPin);
+
+        pinTextField = new JPasswordField();
+        pinTextField.setBounds(130,240,400,40);
+        add(pinTextField);
+
+        login = new JButton("Login");
+        login.setBounds(170, 320, 150, 30);
+        login.setBackground(Color.BLACK);
+        login.setForeground(Color.WHITE);
+        login.addActionListener(this);
+        add(login);
+
+        register = new JButton("Register");
+        register.setBounds(340, 320, 150, 30);
+        register.setBackground(Color.BLACK);
+        register.setForeground(Color.WHITE);
+        register.addActionListener(this);
+        add(register);
+
+        clear = new JButton("Clear");
+        clear.setBounds(250, 370, 150, 30);
+        clear.setBackground(Color.BLACK);
+        clear.setForeground(Color.WHITE);
+        clear.addActionListener(this);
+        add(clear);
+
+//        getContentPane().setBackground(Color.WHITE);
+
+        setSize(650,500); // setting size of frame
+        setVisible(true); // visibility on
+        setLocation(450, 200);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // What to do if some buttons is clicked
+        if(e.getSource() == login){
+
+        }else if(e.getSource() == register){
+
+        }else if(e.getSource() == clear){
+            cardTextField.setText("");
+            pinTextField.setText("");
+        }
+    }
+    public static void main(String[] args) {
+        new Login();
+    }
+
+
+}
